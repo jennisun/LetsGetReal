e710c0a3d2b4b75fad7cde59ddc3553498a21b35
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-11-22 20:25:12 -0500

    equals

---
+++ tester.java
@@ -0,0 +1,13 @@
+public class tester{
+  public static void main(String[] args) {
+    RealNumber a = new RealNumber(0);
+    RealNumber b = new RealNumber(0);
+    System.out.println(a.equals(b));
+
+
+
+  }
+
+
+
+}
