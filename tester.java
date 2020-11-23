3985bc2dcf2ac76965fdb73d1b864e2eddc8d056
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-11-22 20:47:41 -0500

    test

--- tester.java
+++ tester.java
@@ -1,8 +1,13 @@
 public class tester{
   public static void main(String[] args) {
-    RealNumber a = new RealNumber(0);
-    RealNumber b = new RealNumber(0);
-    System.out.println(a.equals(b));
+    RealNumber a = new RealNumber(2);
+    RealNumber b = new RealNumber(-15.5);
+    RealNumber c = new RealNumber(0);
+    System.out.println(a.add(b));
+    System.out.println(a.multiply(b));
+    System.out.println(a.divide(b));
+    System.out.println(a.divide(c));
+    System.out.println(a.subtract(c));
 
 
 
