427f3c0a74714ec54224c75a416e5665901248a3
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-11-22 20:28:39 -0500

    add

--- RealNumber.java
+++ RealNumber.java
@@ -30,16 +30,14 @@ public class RealNumber{
     }
   }
 
-  // /*
-  // *Return a new RealNumber that has the value of:
-  // *the sum of this and the other
-  // */
-  // public RealNumber add(RealNumber other){
-  //    //other can be ANY RealNumber, including a RationalNumber
-  //    //or other subclasses of RealNumber (that aren't written yet)
-  //    return null;
-  // }
-  //
+  /*
+  *Return a new RealNumber that has the value of:
+  *the sum of this and the other
+  */
+  public RealNumber add(RealNumber other){
+     return new RealNumber (this.getValue() + other.getValue());
+  }
+
   // /*
   // *Return a new RealNumber that has the value of:
   // *the product of this and the other
